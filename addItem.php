48299fd934b678cd00cfdf4978834c1c8578f56f
Martin Petiot <73895766+MartinPetiot@users.noreply.github.com>
2020-12-07 11:37:46 +0100

    partie1

--- addItem.php
+++ addItem.php
@@ -1,1 +1,18 @@
-<?php
+
+<html>
+<body>
+<form method="post" action="displayItem.php" enctype=multipart/form-data">
+
+    <p> Choisissez votre article :
+    </p>
+    <select name="article">
+        <option value="article1"> Mamamia</option>
+        <option value="article2"> Aïe Aïe</option>
+        <option value="article3"> Muy Bonita</option>
+    </select>
+
+    <input type="submit" value="Valider" />
+
+</form>
+</body>
+</html>
