562eb1adc9025f6b27e4ecacde6ab7ede58ebe5b
Martin Petiot <73895766+MartinPetiot@users.noreply.github.com>
2020-12-08 11:44:26 +0100

    exofini?

--- displayItem.php
+++ displayItem.php
@@ -1,4 +1,14 @@
 <?php
-echo $_POST["Prix"];
-echo $_POST["Nom"];
-echo "<img src=".$_POST['Photo']." >";
+$valide = " ";
+if (!empty($_POST["nom"]) AND !empty($_POST["photo"]) AND $_POST["prix"]>0) {
+$valide = "le produit est bon " . htmlspecialchars($_POST["nom"]) . " " .  htmlspecialchars($_POST["prix"]) . " € : " . "<br>" . "<img src=".$_POST['photo']." >" ;
+}
+else {
+    header("Location: http://localhost/php5/addItem.php?iserror=true");
+    exit();
+
+}
+echo $valide;
+?>
+
+
